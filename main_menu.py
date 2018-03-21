10a789bfd34e40e8a7bd4bdcdd0e39096ebc56cf
Marat Gadzhibutaev <rabbitjkee@gmail.com>
2018-03-21 18:06:31 +0300

    добавил рандомный довод на получение знаний на главный экран

--- main_menu.py
+++ main_menu.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from random import randrange
 
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 Builder.load_string('''
 <MainMenu>:
@@ -21,7 +23,7 @@ Builder.load_string('''
         Label:
             id: knowledge_evidence
             text_size: self.size
-            text: 'ayasdah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis, ayah, hadis'
+            text: root.knowledge_evidence
 
         Button:
             id: learn_questions
@@ -38,4 +40,21 @@ Builder.load_string('''
 
 
 class MainMenu(Screen):
-    pass
+    knowledge_evidence = StringProperty()
+    knowledge_evidences = [
+        '«И говори: “Господь мой, прибавь мне знания”»\n(Та ха 20:114)',
+        'Пророк (мир ему и благословение Аллаха) сказал:\n«Достоинство знания выше, '
+        'чем достоинство поклонения, а в основе религии лежит благочестие»\n(аль-Хаким, '
+        'аль-Баззар)',
+        'Посланник Аллаха (да благословит его Аллах и приветствует) сказал:\n«Кто пришёл в мою '
+        'мечеть, желая обучиться благому или обучить благому, тот подобен муджахиду на пути '
+        'Аллаха»\n(Ахмад, Ибн Маджах)',
+        'Посланник Аллаха (мир ему и благословение Аллаха) сказал:\n«Кто пришел в мечеть, '
+        'желая обучиться благому или обучить благому, тому запишется награда человека, '
+        'совершившего полноценный Хадж»\n(ат-Табарани)',
+        'Пророк (мир ему и благословение Аллаха) сказал:\n«Тот, кто вышел на пути поиска знаний, '
+        'находится на пути Аллаха, пока не вернется»\n(ат-Тирмизи, ат-Табарани)'
+    ]
+
+    def on_enter(self):
+        self.knowledge_evidence = self.knowledge_evidences[randrange(len(self.knowledge_evidences))]
